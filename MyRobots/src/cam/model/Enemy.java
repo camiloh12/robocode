60c4299a6107680861d8f70350e099e5789d03f1
camiloh12 <camiloh1286@gmail.com>
2016-09-21 16:50:26 -0400

    Additional data stored for each enemy for better targeting

--- MyRobots/src/cam/model/Enemy.java
+++ MyRobots/src/cam/model/Enemy.java
@@ -6,5 +6,10 @@ public class Enemy {
 	public Point2D.Double position;
 	public double energy;
 	public boolean live;
+	public double velocity;
+	public double heading;
 	public double lateralVelocity;
+	public double oldHeading;
+	public double headingChange;
+	public double bearing;
 }

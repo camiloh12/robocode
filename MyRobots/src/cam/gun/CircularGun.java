b485b6f3b98488b0ac3ecd978fcf68b2be5f0de4
camiloh12 <camiloh1286@gmail.com>
2016-09-21 16:52:36 -0400

    Incomplete version of circular targeting gun

--- MyRobots/src/cam/gun/CircularGun.java
+++ MyRobots/src/cam/gun/CircularGun.java
@@ -1,9 +1,15 @@
 package cam.gun;
 
+import java.awt.geom.Point2D;
+
 import cam.model.BattleInfo;
 import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
 
+/**
+ * The code for this class is based on the Circular Targeting Tutorial
+ */
 public class CircularGun implements Gun {
 
 	private static CircularGun gun;
@@ -12,6 +18,8 @@ public class CircularGun implements Gun {
 
 	public BattleInfo info;
 	public double bulletPower;
+
+	private double oldEnemyHeading;
 
 	private CircularGun(AdvancedRobot robot) {
 		this.robot = robot;
@@ -26,7 +34,37 @@ public class CircularGun implements Gun {
 
 	@Override
 	public void onScannedRobot(ScannedRobotEvent event) {
-		// TODO Auto-generated method stub
+		double bulletPower = 2.0;
+		double myX = robot.getX();
+		double myY = robot.getY();
+		double absoluteBearing = robot.getHeadingRadians() + event.getBearingRadians();
+		double enemyX = robot.getX() + event.getDistance() * Math.sin(absoluteBearing);
+		double enemyY = robot.getY() + event.getDistance() * Math.cos(absoluteBearing);
+		double enemyHeading = event.getHeadingRadians();
+		double enemyHeadingChange = enemyHeading - oldEnemyHeading;
+		double enemyVelocity = event.getVelocity();
+		oldEnemyHeading = enemyHeading;
+
+		double deltaTime = 0;
+		double battleFieldHeight = robot.getBattleFieldHeight(), battleFieldWidth = robot.getBattleFieldWidth();
+		double predictedX = enemyX, predictedY = enemyY;
+		while ((++deltaTime) * (20.0 - 3.0 * bulletPower) < Point2D.Double.distance(myX, myY, predictedX, predictedY)) {
+			predictedX += Math.sin(enemyHeading) * enemyVelocity;
+			predictedY += Math.cos(enemyHeading) * enemyVelocity;
+			enemyHeading += enemyHeadingChange;
+			if (predictedX < 18.0 || predictedY < 18.0 || predictedX > battleFieldWidth - 18.0
+					|| predictedY > battleFieldHeight - 18.0) {
+
+				predictedX = Math.min(Math.max(18.0, predictedX), battleFieldWidth - 18.0);
+				predictedY = Math.min(Math.max(18.0, predictedY), battleFieldHeight - 18.0);
+				break;
+			}
+		}
+		double theta = Utils.normalAbsoluteAngle(Math.atan2(predictedX - robot.getX(), predictedY - robot.getY()));
+
+		robot.setTurnRadarRightRadians(Utils.normalRelativeAngle(absoluteBearing - robot.getRadarHeadingRadians()));
+		robot.setTurnGunRightRadians(Utils.normalRelativeAngle(theta - robot.getGunHeadingRadians()));
+		robot.fire(bulletPower);
 
 	}
 

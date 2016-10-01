3e1981b134986b8273af077afc51597cbf402375
camiloh12 <camiloh1286@gmail.com>
2016-09-30 20:46:28 -0400

    Removed todos from combined movement

--- MyRobots/src/cam/movement/CombinedMovement.java
+++ MyRobots/src/cam/movement/CombinedMovement.java
@@ -25,8 +25,7 @@ public class CombinedMovement implements Movement {
 	private static final double WALL_STICK = 160;
 	private static final int BINS = 47;
 	/**
-	 * Math.PI/2 would be perpendicular movement, less will keeps us moving away
-	 * slightly
+	 * Math.PI/2 would be perpendicular movement, less will keeps us moving away slightly
 	 */
 	private static final double LESS_THAN_HALF_PI = 1.25;
 
@@ -58,7 +57,7 @@ public class CombinedMovement implements Movement {
 
 	@Override
 	public void onScannedRobot(ScannedRobotEvent event) {
-		// TODO WSMovement
+		// Wave Surfing
 		if (robot.getOthers() <= 1) {
 			myLocation = new Point2D.Double(robot.getX(), robot.getY());
 
@@ -99,7 +98,7 @@ public class CombinedMovement implements Movement {
 	public void update() {
 
 		if (robot.getOthers() > 1) {
-			// TODO MRMovement
+			// Minimum Risk Movement
 			double distanceToNextDestination = info.myLocation.distance(info.nextDestination);
 			double distanceToTarget = info.myLocation.distance(info.target.position);
 
@@ -117,12 +116,9 @@ public class CombinedMovement implements Movement {
 
 				for (int i = 0; i < 200; i++) {
 					/**
-					 * Calculate the testPoint somewhere around the current
-					 * position. 100 + 200*Math.random() proved to be good if
-					 * there are around 10 bots in a 1000x1000 field. But this
-					 * needs to be limited to the distanceToTarget*0.8. This way
-					 * the bot won't run into the target (should be the closest
-					 * bot)
+					 * Calculate the testPoint somewhere around the current position. 100 + 200*Math.random() proved to
+					 * be good if there are around 10 bots in a 1000x1000 field. But this needs to be limited to the
+					 * distanceToTarget*0.8. This way the bot won't run into the target (should be the closest bot)
 					 */
 					testPoint = RoboUtils.project(info.myLocation, 2 * Math.PI * Math.random(),
 							Math.min(distanceToTarget * 0.8, 100 + 200 * Math.random()));
@@ -149,7 +145,7 @@ public class CombinedMovement implements Movement {
 				robot.setMaxVelocity(Math.abs(angle) > 1 ? 0 : 8d);
 			}
 		} else {
-			// TODO WSMovement
+			// Wave Surfing
 			myLocation = new Point2D.Double(robot.getX(), robot.getY());
 			updateWaves();
 			doSurfing();
@@ -158,7 +154,6 @@ public class CombinedMovement implements Movement {
 
 	@Override
 	public void updateWaves(Bullet bullet) {
-		// TODO WSMovement
 		if (robot.getOthers() <= 1) {
 			// If the enemyWaves collection is empty, we must have missed the
 			// detection of this wave somehow.
@@ -196,16 +191,12 @@ public class CombinedMovement implements Movement {
 		while (enumeration.hasMoreElements()) {
 			Enemy enemy = enumeration.nextElement();
 			/**
-			 * - Math.min(enemy.energy/myEnergy,2) is multiplied because
-			 * enemy.energy/myEnergy is an indicator of how dangerous an enemy
-			 * is - Math.abs(Math.cos(calAngle(myLocation,point) -
-			 * calcAngle(enemy.position,point))) is bigger if the moving
-			 * direction isn't good in relation to a certain bot. It would be
-			 * more natural to use Math.abs(
-			 * Math.cos(calcAngle(point,myLocation) - calcAngle(enemy.poistion,
-			 * myLocation))) but this wasn't going to give me good results - 1 /
-			 * point.distanceSq(enemy.position) is just the normal anti gravity
-			 * thing
+			 * - Math.min(enemy.energy/myEnergy,2) is multiplied because enemy.energy/myEnergy is an indicator of how
+			 * dangerous an enemy is - Math.abs(Math.cos(calAngle(myLocation,point) - calcAngle(enemy.position,point)))
+			 * is bigger if the moving direction isn't good in relation to a certain bot. It would be more natural to
+			 * use Math.abs( Math.cos(calcAngle(point,myLocation) - calcAngle(enemy.poistion, myLocation))) but this
+			 * wasn't going to give me good results - 1 / point.distanceSq(enemy.position) is just the normal anti
+			 * gravity thing
 			 */
 			if (enemy.live) {
 				eval += Math.min(enemy.energy / info.myEnergy, 2)
